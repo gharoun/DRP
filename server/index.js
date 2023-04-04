d3eee15479a655b47fe1cf5f28178f51e67af4c5
Gaya Haroun <gharoun94@gmail.com>
2023-04-03 19:09:43 -0500

    test

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
+
+import connectDB from "./mongodb/connect.js";
 import userRouter from "./routes/user.routes.js";
 import propertyRouter from "./routes/property.routes.js";
-import connectDB from "./mongodb/connect.js";
 
 dotenv.config();
 
